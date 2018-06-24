fa9a0a66c13389030b6652be84e18623a8ad6fbc
Gustavo Avena <ggavena@gmail.com>
2018-06-24 15:20:46 -0300

    automated the entire cross validation process, with figures and metrics being saved automatically on the output folder

--- prophet.py
+++ prophet.py
@@ -9,6 +9,8 @@ from datetime import date
 
 import dataset_preparation
 
+DPI = 150
+
 
 def get_dataset_duration(df):
 	# print(df.iloc[0]['ds'])
@@ -23,43 +25,41 @@ def get_dataset_duration(df):
 
 
 
-def test_prophet(train_df, test_df):
-
-	print(train_df.head())
-	print(train_df.tail())
+def fit_model_with_prophet(df, fname, future_period=730):
+	print(df.head())
+	print(df.tail())
 	
-	full_m = Prophet()
-	full_m.fit(test_df)
 
 
 	m = Prophet()
-	m.fit(train_df)
-
-	from pprint import pprint
+	m.fit(df)
 
 
-	future = m.make_future_dataframe(periods=730)
-	# print(future.tail())
+	future = m.make_future_dataframe(periods=future_period)
+	print(future.tail())
 
 
 
-	forecast = m.predict(test_df)
+	forecast = m.predict(future)
 	print(forecast[['ds', 'yhat', 'yhat_lower', 'yhat_upper']].tail())
 
 	fig1 = m.plot(forecast)
 	fig1.suptitle('{} prediction model'.format(fname))
 	
+	fig1.figsize = (1920/DPI, 1080/DPI)
+	fig1.savefig('Figure_111.png', dpi=DPI)
+	# plt.show(fig1)
 
-	plt.show(fig1)
 
 
+def prophet_cross_validation(df, fname, initial_ratio=0.6, period_ratio=0.05, horizon_ratio=0.1, rolling_window=0.1):
+	output_path = 'output/prophet'
+	dpi = 150
 
-def test_prophet_cross_validation(df, fname, initial_ratio=0.6, period_ratio=0.05, horizon_ratio=0.1, rolling_window=0.1):
-	output_path = '../output/prophet'
+	print(fname.replace('.csv', '_{}_{}_{}.csv'.format(initial_ratio, period_ratio, horizon_ratio)))
 
 	print("Performing cross validation on file: ", fname)
-	# print(df.head())
-	# print(df.tail())
+
 	
 	duration = get_dataset_duration(df)
 
@@ -73,28 +73,43 @@ def test_prophet_cross_validation(df, fname, initial_ratio=0.6, period_ratio=0.0
 	m = Prophet()
 	m.fit(df)
 
-	# df_cv = diagnostics.cross_validation(m, initial='1825 days', period='180 days', horizon = '365 days')
+	
+
+
 	df_cv = diagnostics.cross_validation(m, initial=initial, period=period, horizon=horizon)
-	# print(df_cv)
 
-	df_cv.to_csv(os.path.join(output_path, fname.replace('.csv', '_cross_validation.csv')))
+	
 
-	# print(df_cv)
 
 	# performance metrics
 	df_p = diagnostics.performance_metrics(df_cv, rolling_window=rolling_window)
 	print(df_p)
 
-	df_p.to_csv(os.path.join(output_path, fname.replace('.csv', '_performance_metrics.csv')))
+
 
 
 	fig1 = plot_cross_validation_metric(df_cv, metric='mape', rolling_window=rolling_window)
 	fig1.suptitle('{} cross_validation MAPE'.format(fname))
-	plt.show(fig1)
+	fig1.figsize = (1920/DPI, 1080/DPI)
 
-	# fig3 = plot_cross_validation_metric(df_cv, metric='mse')
-	# fig2.suptitle('{} cross_validation MSE'.format(fname))
-	# plt.show(fig3)
+	# saving files...
+	suffix = '_{}_{}_{}'.format(initial_ratio, period_ratio, horizon_ratio)
+	output_folder = os.path.join(output_path, 'cv' + suffix)
+
+	if(not os.path.exists(output_folder)):
+		print('creating output folder: ', output_folder)
+		os.mkdir(output_folder)
+
+
+	df_cv.to_csv(os.path.join(output_folder, fname.replace('.csv', suffix + '_cross_validation.csv')))
+
+	df_p.to_csv(os.path.join(output_folder, fname.replace('.csv', suffix + '_performance_metrics.csv')))
+
+	figure_output = os.path.join(output_folder, fname.replace('.csv', suffix + '_mape.png'))
+	fig1.savefig(figure_output, dpi=DPI)
+	# plt.show(fig1)
+
+
 
 def main():
 
@@ -105,10 +120,10 @@ def main():
 		fname = 'PETR4.SA.csv'
 
 	full_df = dataset_preparation.get_full_dataframe(fname)
-	train_df, test_df = dataset_preparation.get_train_test_dataframe(fname)
+	# train_df, test_df = dataset_preparation.get_train_test_dataframe(fname)
 
-	# test_prophet(train_df, full_df)
-	test_prophet_cross_validation(full_df, fname)
+	# fit_model_with_prophet(full_df, fname)
+	prophet_cross_validation(full_df, fname)
 
 
 
